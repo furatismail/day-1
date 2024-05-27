03e0a782ac2e6d6b5d79e8c0de962e4b7e7f51a5
Furát Ismail <furat.ismail@cadcom.cz>
2024-05-27 12:57:48 +0200

    preloading

---
+++ src/app/features/control-panel/control-panel.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from "@angular/router";
+import { StatisticsComponent } from "./statistics/statistics.component";
+
+export const ControlPanelRoutes: Routes = [
+    {
+        path: "",
+        loadComponent: () => import('./overview/overview.component').then((component) => component.OverviewComponent),
+        children: [
+            {
+                path: 'settings',
+                loadComponent: () => import('./settings/settings.component').then((component) => component.SettingsComponent)
+            },
+            {
+                path: 'settings',
+                component: StatisticsComponent
+            }
+        ]
+    }
+]
