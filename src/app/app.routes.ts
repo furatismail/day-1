1ed39f94170d58871733590b6f0caeb7e56097ce
Furát Ismail <furat.ismail@cadcom.cz>
2024-05-27 10:23:17 +0200

    load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: "dashboard",
+        pathMatch: 'full'
+    },
+    {
+        path: "dashboard",
+        component: DashboardComponent
+    },
+    {
+        path: "about/team",
+        loadComponent: () => import('./features/about/team/team.component').then((component) => component.TeamComponent)
+    }
+];
