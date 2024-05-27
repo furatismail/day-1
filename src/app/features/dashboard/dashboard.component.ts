d174abeeced7b32a25d42e33447da0a338745652
Furát Ismail <furat.ismail@cadcom.cz>
2024-05-27 10:29:51 +0200

    changes

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  private router = inject(Router)
+  navigateToAbout() : void {
+    this.router.navigate(['/about/team'])
+  }
 }
